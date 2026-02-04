1c2e03ca5dd46f2812cd90d0c89f24822c57d8ba
Volodymyr Noha <volodymyrnoha99@gmail.com>
2026-02-04 14:10:13 +0200

    lib: Add Adafruit_GFX screen library
    Signed-off-by: Volodymyr Noha <volodymyrnoha99@gmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,12 @@
 #include <Arduino.h>
 
+/* Include Adafruit GFX library */
+#include "RGBmatrixPanel.h"
+#include "bit_bmp.h"
+#include "fonts.h"
+#include <string.h>
+#include <stdlib.h>
+
 // put function declarations here:
 int myFunction(int, int);
 
